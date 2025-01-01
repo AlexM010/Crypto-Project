cd42b2abb3ae3d358864e9168957f7037a5fb3e5
PercySS <jimmy.makro24@gmail.com>
2025-01-01 14:27:26 +0200

    checking i can reach all items in mongo (shell)

--- src/replacer.py
+++ src/replacer.py
@@ -16,22 +16,42 @@ replacement_map = {
     "ECB_Mode": "CBC with random IV",
     "RSA_512_1024": "RSA-2048",
     "AES_128": "AES-256",
-    "AES_192": "AES-256",
-    
+    "AES_192": "AES-256",  
 }
+
+def print_scans():
+    for scan in scans_collection.find():
+        print(scan["scan_id"], scan["date"])
+        
+def print_chosen_scan(scan):
+    # print the id
+    print("ID:", scan["scan_id"])
+    # print the date
+    print("Date:", scan["date"])
+    # print how many files scanned 
+    print("Files scanned:", len(scan["files_scanned"]))
+    # print the vulnerable files 
+    print("Vulnerable files:", len(scan["vulnerable_files"]))
 
 # connect to mongodb
 client = MongoClient("mongodb://localhost:27017/")
 db = client["cryptographic_inventory"]
 scans_collection = db["scans"]
+    
+while True:
+    print_scans()
+    scan_id = input("Enter the scan id: ").strip()
+    scan = scans_collection.find_one({"scan_id": scan_id})
+    
+    if scan:
+        break
+    else:
+        print("Invalid scan id")
 
-# get input for the scans by id
-def get_scans_by_id():
-    scan_id = input("Enter the scan id: ")
-    # print all scans in  format id: name
-    for scan in scans_collection.find():
-        print(scan["_id"], scan["name"])  
-        
-    return scans_collection.find_one({"_id": scan_id})
+print("Scan id: ", scan["scan_id"], "Scan date:", scan["date"])
 
+print_chosen_scan(scan)
+
+
+    
 
